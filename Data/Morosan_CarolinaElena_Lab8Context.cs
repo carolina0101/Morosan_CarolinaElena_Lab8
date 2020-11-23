6d0f05b58e38c8743adab68ff4a4550f6aa9e230
CAROLINA-ELENA MOROSAN <morosan.carolina165217@student.ubbcluj.ro>
2020-11-23 17:55:04 +0200

    Add project files.

---
+++ Data/Morosan_CarolinaElena_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Morosan_CarolinaElena_Lab8.Models;
+
+namespace Morosan_CarolinaElena_Lab8.Data
+{
+    public class Morosan_CarolinaElena_Lab8Context : DbContext
+    {
+        public Morosan_CarolinaElena_Lab8Context (DbContextOptions<Morosan_CarolinaElena_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Morosan_CarolinaElena_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Morosan_CarolinaElena_Lab8.Data;
+using Morosan_CarolinaElena_Lab8.Models;
+
+namespace Morosan_CarolinaElena_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Morosan_CarolinaElena_Lab8.Data.Morosan_CarolinaElena_Lab8Context _context;
+
+        public IndexModel(Morosan_CarolinaElena_Lab8.Data.Morosan_CarolinaElena_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
